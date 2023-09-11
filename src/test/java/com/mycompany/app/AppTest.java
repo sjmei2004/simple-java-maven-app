c746eb3a0ed849432a8b05a29575b0f217bd0bc4
sunjunmei <184036895@qq.com>
2023-09-11 21:54:54 +0800

    modifed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -21,5 +21,6 @@ public class AppTest
     {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello World!", app.getMessage());
     }
 }
